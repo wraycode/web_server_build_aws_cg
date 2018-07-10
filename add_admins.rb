be11cb3c3cab6df41477f3a856efdd1e129ef203
Jeremy Barton <jbarton@circlegraphicsonline.com>
2018-07-10 10:34:32 -0600

    updated

--- add_admins.rb
+++ add_admins.rb
@@ -6,10 +6,13 @@ json = file.read
 file.close
 admins = JSON.parse(json)
 for admin in admins
-    puts admin['name'] 
-    puts admin['key']     
-    auth_file = File.open("/home/"+admin['name']+"/.ssh/authorized_keys",'a')
-    auth_file.write(admin['key'])    
-    auth_file.close
+    #puts admin['name'] 
+    #puts admin['key']  
+    wasGood = system("./add_admin.sh admin['name']"] )   
+    if wasGood
+	    auth_file = File.open("/home/"+admin['name']+"/.ssh/authorized_keys",'a')
+	    auth_file.write(admin['key'])    
+	    auth_file.close
+	end    
 end    
 
