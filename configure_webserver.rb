297cb5be052d9dae83e46be34083071da5a78013
Jeremy Barton <jbarton@circlegraphicsonline.com>
2018-07-11 11:01:13 -0600

    update

--- configure_webserver.rb
+++ configure_webserver.rb
@@ -3,23 +3,25 @@ require 'json'
 
 
 
+
 def prompt(*args)
   print(*args)
   result = gets.strip
   return result.empty? ? 'error' : result
-end   
+end
 
-#serverName = prompt "Enter serverName: "
+serverName = prompt "Enter serverName: "
 #print serverName+"\n\r"
 #projectRepository = prompt "Enter Project Repository: "
 #print projectRepository+"\n\r"
 #system("rm /var/www/html/*")
 Dir.chdir "/var/www/html/"
 #status = system("git clone "+projectRepository+"")   
-   
-web_root = system("ls /var/www/html")  
-print web_root 
 
-#default_000 = File.open("/etc/apache2/sites-ava",'a')
-system('sudo sed -i "DocumentRoot/c\DocumentRoot /var/www/html/'+web_root+'" /var/www/html/'+web_root)
+#web_root = system("ls /var/www/html")  
+web_root = %x[ls /var/www/html]
+print web_root
+
+system('sudo sed -i "/DocumentRoot/c\        DocumentRoot /var/www/html/'+web_root+'" /etc/apache2/sites-available/000-default.conf')
+system('sudo sed -e "/DocumentRoot /var/www/html/'+web_root+'/a\" -e "ServerName '+serverName+'" /etc/apache2/sites-available/000-default.conf')
 
