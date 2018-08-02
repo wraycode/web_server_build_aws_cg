61201359fc099cebfc27261b3be36097f2eefe53
Ubuntu <ubuntu@signage-ups.com>
2018-08-02 16:47:38 +0000

    update

--- configure_webserver.rb
+++ configure_webserver.rb
@@ -15,24 +15,28 @@ system('sudo chown -R www-data:www-data /var/www/html/')
 system('sudo chmod -R 775 /var/www/html/')
 system("sudo rm /var/www/html/*")
 Dir.chdir "/var/www/html/"
-status = system("git clone "+projectRepository+")   
-web_root = %x[ls /var/www/html]
+status = system("git clone "+projectRepository)   
+web_root = %x["ls /var/www/html"]
 print web_root
+
+
+
+
 
 system('sudo sed -i "/DocumentRoot/c\        DocumentRoot /var/www/html/'+web_root+'" /etc/apache2/sites-available/000-default.conf')
 system('sudo sed -e "/DocumentRoot /var/www/html/'+web_root+'/a\" -e "ServerName '+serverName+'" /etc/apache2/sites-available/000-default.conf')
 system('sudo service apache2 restart')
 
-#print 'setting up letsencrypt ssl certs'
-#openedPorts =promt "Certbot needs to have access to the server. Have the ports been opened up for http and https? (y/n)"
-#if openedPorts.downcase == 'y' then
+# print 'setting up letsencrypt ssl certs'
+# openedPorts =promt "Certbot needs to have access to the server. Have the ports been opened up for http and https? (y/n)"
+# if openedPorts.downcase == 'y' then
 #	system('sudo ./letsEncrypt')
-#else
+# else
 #	puts "Ports will need to be opened before Certbot can setup the ssl certificates"
-#end	
+# end	
 puts Dir.getwd
-system("sudo chown -R www-data:www-data /var/www/html/#{web_root}")
+system("sudo chown -R www-data:www-data /var/www/html/"+web_root.chomp)
 Dir.chdir(web_root.chomp)
 system('composer install')
-#print "setting up apache basic authintication"
-#system('sudo apt-get install apache2-utils')
+# print "setting up apache basic authintication"
+system('sudo apt-get install apache2-utils')
