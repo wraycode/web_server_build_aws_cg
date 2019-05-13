83cb3d99547974ba566767dbd8e0e7346ad3e8ac
wraycode <wraycode@gmail.com>
2019-05-13 07:01:20 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -27,8 +27,8 @@
      sudo apt-get install -y apache2 zip unzip curl git
 
      ## start install php ##
-     sudo apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-xml php7.0-mbstring php7.0-curl php7.0-mcrypt \
-	     php-imagick php7.0-gd php-memcache php7.0-xmlrpc php7.0-zip ruby vim  
+     sudo apt-get install -y php libapache2-mod-php7.2 php7.2-mysql php7.2-xml php7.2-mbstring php7.2-curl php7.2-mcrypt \
+	     php-imagick php7.2-gd php-memcache php7.2-xmlrpc php7.2-zip ruby vim  
 
      ## install compser ##
      curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
